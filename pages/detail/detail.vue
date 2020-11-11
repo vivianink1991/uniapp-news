16a787c5658f2e5e89535b41ceab825f92f51c23
jiaxin.j.li <jiaxin.j.li@daimler.com>
2020-11-11 17:32:16 +0800

    first commit

---
+++ pages/detail/detail.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="news-detail">
+		<view class="news-hd">{{title}}</view>
+		<view class="news-bd">
+			<rich-text :nodes="body"></rich-text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '',
+				body: '',
+			}
+		},
+		onLoad(e) {
+			console.log(e)
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news/36kr/' + e.newsId,
+				method: 'GET',
+				data: {},
+				success: (res) => {
+					const {title, content} = res.data
+					this.title = title
+					this.body = content
+				}
+			})
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.news-detail {
+	padding: 60upx 40upx;
+}
+.news-hd {
+	text-align: center;
+	font-size: 16px;
+	font-weight: 600;
+	margin-bottom: 40upx;
+}
+.news-bd {
+	color: #666;
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,84 @@
+<template>
+	<view class="content">
+		<view class="uni-list-news">
+			<view class="uni-list-item"
+				  v-for="(item, index) in list"
+				  :key="index"
+				  @tap="goToDetail"
+				  :data-newsid="item.post_id">
+				<image :src="item.author_avatar" class="uni-item-img"></image>
+				<view class="uni-item-content">
+					<view class="uni-item-title">{{item.title}}</view>
+					<view class="uni-item-bd">{{item.created_at}}</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: []
+			}
+		},
+		onLoad() {
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news',
+				method:'GET',
+				success: res => {
+					this.list = res.data || []
+					// console.log(res.data)
+				},
+				fail: err => {
+					
+				}
+			})
+		},
+		methods: {
+			goToDetail(e) {
+				console.log(e)
+				var newsId = e.currentTarget.dataset.newsid
+				uni.navigateTo({
+					url: '../detail/detail?newsId=' + newsId
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+.uni-list-news {
+	padding: 0upx 20upx;
+}
+.uni-list-item {
+	display: flex;
+	justify-content: space-between;
+	margin-top: 40upx;
+	padding: 20upx 0upx;
+	border-bottom: 1px solid #eee;
+}
+.uni-item-img {
+	display: block;
+	width: 100upx;
+	height: 100upx;
+	margin-right: 60upx;
+	border-radius: 100%;
+}
+.uni-item-content {
+	flex: 1;
+	text-align: left;
+	/* overflow: hidden; */
+}
+.uni-item-title {
+	/* overflow: hidden;
+	text-overflow: ellipsis;
+	white-space: nowrap; */
+}
+.uni-item-bd {
+	color: #aaa;
+	margin-top: 10upx;
+	font-size: 12px;
+}
+</style>
